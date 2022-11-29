50a1009216931bc2ac3119b1526af047106236f7
kabutokunnDX <96281796+kabutokunnDX@users.noreply.github.com>
2022-11-29 10:09:08 +0900

    snakeyamlとかいう変なの消した

--- src/main/java/kbtdx/links/man10linkplugin/urlop.java
+++ src/main/java/kbtdx/links/man10linkplugin/urlop.java
@@ -11,7 +11,6 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.IOException;
